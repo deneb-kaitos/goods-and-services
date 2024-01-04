a5386d67a47e234912e8bfa72805a89210163fa0
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2024-01-04 09:36:49 +0100

    added Header/Footer

---
+++ sources/front-end/owner/src/lib/containers/Footer/Footer.svelte
@@ -0,0 +1,13 @@
+<style>
+  footer {
+    grid-area: footer;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 4rem;
+  }
+</style>
+
+<footer>g&s</footer>
---
+++ sources/front-end/owner/src/lib/containers/Header/Header.svelte
@@ -0,0 +1,18 @@
+<style>
+  header {
+    grid-area: header;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  header > h1 {
+    line-height: normal;
+    color: var(--theme-black);
+  }
+</style>
+
+<header>
+  <h1>G&S</h1>
+</header>
